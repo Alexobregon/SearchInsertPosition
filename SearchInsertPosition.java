2aa1dafa40051f8b84b720e3a589d37e9582a77c
Alexander Obregon <alexander.obregon47@gmail.com>
2023-03-02 20:47:07 -0800

    added while statement

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -1,6 +1,8 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        
+        int low = 0;
+        int high = nums.length -1;
+        int pointer = high / 2;
         return 1;
     }
 }
