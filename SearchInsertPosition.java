f28c0b865d795006ccf47b94acee5210fa56e5c6
Alexander Obregon <alexander.obregon47@gmail.com>
2023-03-02 20:49:09 -0800

    added else if statement

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -3,6 +3,14 @@ class Solution {
         int low = 0;
         int high = nums.length -1;
         int pointer = high / 2;
+        while (low <= high){
+         
+            if (nums[pointer] == target) {
+                return pointer;
+            }  else if(target > nums[pointer]){
+                low = pointer +1;
+            }
+        }
         return 1;
     }
 }
