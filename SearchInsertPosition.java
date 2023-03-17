c90184bd05e329fe1f2c0c55af1eeb516a5785f0
Alexander Obregon <alexander.obregon47@gmail.com>
2023-03-16 19:56:25 -0700

    added init comments

--- SearchInsertPosition.java
+++ SearchInsertPosition.java
@@ -1,5 +1,6 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
+        // Initialize the lower and upper bounds of the search range
         int low = 0;
         int high = nums.length -1;
         int pointer = high / 2;
